a9ec3cc21d4cf7a9a11cf78d2eac490ec95425f4
JacquelineMG <j.gilchrist@outlook.com>
2024-07-28 08:32:04 -0700

    handle messages from the server

--- WK5/snake-client/play.js
+++ WK5/snake-client/play.js
@@ -8,6 +8,14 @@ const connect = () => {
 
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("You are connected!");
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says:", data);
+  });
+
   return conn;
 };
 
