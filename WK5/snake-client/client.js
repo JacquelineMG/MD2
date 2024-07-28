53702c2a072e785f47b6f384d148d16f8d70e24f
JacquelineMG <j.gilchrist@outlook.com>
2024-07-28 08:36:51 -0700

    move connect() to separate file

---
+++ WK5/snake-client/client.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("You are connected!");
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says:", data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
--- WK5/snake-client/play.js
+++ WK5/snake-client/play.js
@@ -1,23 +1,4 @@
-const net = require("net");
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("You are connected!");
-  });
-
-  conn.on("data", (data) => {
-    console.log("Server says:", data);
-  });
-
-  return conn;
-};
+const connect = require("./client");
 
 console.log("Connecting...");
 
