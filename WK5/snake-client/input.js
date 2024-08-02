83e08a247401e6e8151508ab6d0d060cdf8e5c0a
JacquelineMG <j.gilchrist@outlook.com>
2024-08-02 09:56:11 -0700

    move input functions to new input file

---
+++ WK5/snake-client/input.js
@@ -0,0 +1,21 @@
+const handleUserInput = key => {
+  if (key === "w") {
+    console.log("WWWWW");
+  }
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+module.exports = { setupInput };
