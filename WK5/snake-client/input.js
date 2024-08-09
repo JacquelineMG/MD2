3876995fb143cfabfc9cdef68b4fedd00b582d7b
JacquelineMG <j.gilchrist@outlook.com>
2024-08-09 08:37:49 -0700

    implement message keys

--- WK5/snake-client/input.js
+++ WK5/snake-client/input.js
@@ -1,24 +1,40 @@
 let connection;
 
 const handleUserInput = key => {
-  if (key === "w") {
+  if (key === "w" || key === "W") {
     connection.write("Move: up");
   }
 
-  if (key === "a") {
+  if (key === "a" || key === "A") {
     connection.write("Move: left");
   }
 
-  if (key === "s") {
+  if (key === "s" || key === "S") {
     connection.write("Move: down");
   }
 
-  if (key === "d") {
+  if (key === "d" || key === "D") {
     connection.write("Move: right");
   }
 
   if (key === "\u0003") {
     process.exit();
+  }
+
+  if (key === "p" || key === "P") {
+    connection.write("Say: It's sithering time!");
+  }
+
+  if (key === "o" || key === "O") {
+    connection.write("Say: SSSSSsssss");
+  }
+
+  if (key === "i" || key === "I") {
+    connection.write("Say: Hisssss");
+  }
+
+  if (key === "l" || key === "L") {
+    connection.write("Say: Making hissstory!");
   }
 };
 
