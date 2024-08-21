3f475ffbeafff41cfc548483460124a4d69ef84a
JacquelineMG <j.gilchrist@outlook.com>
2024-08-21 09:59:15 -0700

    implement vertical search

--- WK5/wordSearch/wordsearch.js
+++ WK5/wordSearch/wordsearch.js
@@ -3,8 +3,25 @@ const wordSearch = (letters, word) => {
   for (let l of horizontalJoin) {
     if (l.includes(word)) return true;
   }
+
+  const transposedArr = [];
+
+  for (let t = 0; t < letters[0].length; t++) {
+    transposedArr.push([]);
+  }
+
+  for (let x = 0; x < letters.length; x++) {
+    for (let y = 0; y < letters[x].length; y++) {
+      transposedArr[y].push(letters[x][y]);
+    }
+  }
+
+  const verticalJoin = transposedArr.map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
   return false;
 };
-
 
 module.exports = wordSearch;
