9d08e0e6eb7b7bcb71287c42534f7593bdeb0c01
JacquelineMG <j.gilchrist@outlook.com>
2024-08-27 12:49:49 -0700

    start client input

--- WK5/file-server/client.js
+++ WK5/file-server/client.js
@@ -13,4 +13,27 @@ conn.on("connect", () => {
   conn.write("Hello from client");
 });
 
+
+
+// const handleUserInput = key => {
+//   if (key === "\u0003") {
+//     process.exit();
+//   } else {
+//     conn.write(key);
+//   }
+// };
+
+// const setupInput = () => {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+
+//   stdin.on("data", handleUserInput);
+
+//   return stdin;
+// };
+
+// setupInput();
+
 conn.setEncoding("utf8");
