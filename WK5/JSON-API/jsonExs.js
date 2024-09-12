59fc01cbe7a53a5371a59a0788ac2788a406752a
JacquelineMG <j.gilchrist@outlook.com>
2024-09-12 10:09:07 -0700

    work on JSON reading examples

--- WK5/JSON-API/jsonExs.js
+++ WK5/JSON-API/jsonExs.js
@@ -7,3 +7,15 @@ console.log(typeof result1);
 console.log(typeof JSON.stringify(result1));
 
 console.log("-----------------------------------------");
+
+const jsonString = `{"a":1, "b":2, "foo":"bar"}`;
+console.log(typeof jsonString, jsonString);
+
+const obj = JSON.parse(jsonString);
+console.log(typeof obj, obj);
+
+delete obj.foo;
+console.log(typeof obj, obj);
+
+const reObj = JSON.stringify(obj)
+console.log(typeof reObj, reObj);
