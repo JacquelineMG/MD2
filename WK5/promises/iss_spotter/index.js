b95de0497f320d87e51614bf54d084881fbbe824
JacquelineMG <j.gilchrist@outlook.com>
2024-10-03 10:16:40 -0700

    get lat and long info

--- WK5/promises/iss_spotter/index.js
+++ WK5/promises/iss_spotter/index.js
@@ -1,10 +1,17 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((err, ip) => {
   if (err) {
     console.log("ERROR:", err);
     return;
   }
-
-  console.log(ip);
+  
+  fetchCoordsByIP(ip, (err, data) => {
+    if (err) {
+      console.log("ERROR:", err);
+      return;
+    }
+  
+    console.log(data);
+  });
 });
--- WK5/promises/iss_spotter/iss.js
+++ WK5/promises/iss_spotter/iss.js
@@ -19,6 +19,23 @@ const fetchMyIP = callback => {
 const fetchCoordsByIP = (ip, callback) => {
   const url = `http://ipwho.is/${ip}`;
 
+  needle.get(url, (err, resp, body) => {
+    if (err) return callback(err, null);
+
+    if (resp.statusCode !== 200) {
+      const msg = `Status Code ${resp.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const latitude = body.latitude;
+    const longitude = body.longitude;
+    const data = { latitude: latitude, longitude: longitude };
+
+    callback(null, data);
+
+  });
+
 };
 
 module.exports = { fetchMyIP, fetchCoordsByIP };
