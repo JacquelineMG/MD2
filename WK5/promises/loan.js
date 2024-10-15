0c63777af63eec1fcd38ca72d2f8ab02aa8b032d
JacquelineMG <j.gilchrist@outlook.com>
2024-10-15 13:19:18 -0700

    set up files for iss with promises

--- WK5/promises/loan.js
+++ WK5/promises/loan.js
@@ -1,15 +1,28 @@
-let creditLimit = 5000;
+let creditLimit = -10;
 
 const loanOut = amount => {
   return new Promise((resolve, reject) => {
-    resolve(amount);
+
+    if (creditLimit <= 0) {
+      reject("Insufficient funds");
+    }
+
+    if (amount < creditLimit) {
+      creditLimit = creditLimit - amount;
+      resolve(amount);
+    }
+
+    if (amount > creditLimit) {
+      resolve(creditLimit);
+      creditLimit = 0;
+    }
   });
 
 };
 
 loanOut(150)
   .then((amountReceived) => {
-    console.log(`Amount received: ${amountReceived}. Remaining credit limit: ${creditLimit}`)
+    console.log(`Amount received: ${amountReceived}. Remaining credit limit: ${creditLimit}`);
   })
   .catch((err) => {
     console.log(`ERROR: ${err}`);
